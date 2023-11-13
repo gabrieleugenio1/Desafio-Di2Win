823b288c416e0bbd59142cfc4c998e308dcfac11
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-11-13 02:21:13 -0300

    feat: route

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
