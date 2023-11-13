e393eafedf0ba589e75d38bed941eec972e96d7c
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-11-13 03:31:07 -0300

    refactor: change css home

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -9,5 +9,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+    imageSrc: string | null = null;
 
+    handleFileInput(event: any): void {
+      const file = event.target.files[0];
+
+      if (file) {
+        this.createImageObjectUrl(file);
+      }
+    }
+
+    createImageObjectUrl(file: File): void {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.imageSrc = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
 }
